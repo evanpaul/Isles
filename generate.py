731a949db62345f88837110731d01ee492aaa0d4
evanpaul22 <evanpaul22@gmail.com>
2018-09-30 01:24:54 -0700

    Add mountain generation; test variable scaling

--- generate.py
+++ generate.py
@@ -19,11 +19,13 @@ class Grid:
         for row in self.tiles:
             for tile in row:
                 if tile == 0:
-                    print(Fore.BLUE + "0" + Style.RESET_ALL, end=" ")
-                if tile == 1:
-                    print(Fore.GREEN + "1" + Style.RESET_ALL, end=" ")
-                if tile == 2:
-                    print(Fore.YELLOW + "2" + Style.RESET_ALL, end=" ")
+                    print(Fore.BLUE + "~" + Style.RESET_ALL, end=" ")
+                elif tile == 1:
+                    print(Fore.GREEN + "-" + Style.RESET_ALL, end=" ")
+                elif tile == 2:
+                    print(Fore.YELLOW + "." + Style.RESET_ALL, end=" ")
+                elif tile == 3:
+                    print(Fore.WHITE + "^" + Style.RESET_ALL, end=" ")
             print()
 
     def get_random_loc(self) -> Tuple[int, int]:
@@ -35,15 +37,47 @@ class Grid:
     def set(self, r, c, value) -> None:
         g.tiles[r][c] = value
 
-    def add_island(self):
+    def generate_island(self):
         r, c = self.get_random_loc()
         i = Island(r, c, self)
         self.islands.append(i)
 
+    def generate_mountains(self):
+        import itertools
+        # for tile in itertools.chain(*self.tiles):
+        for r, row in enumerate(self.tiles):
+            for c, tile in enumerate(row):
+                if tile == 2:
+                    num_water = num_grass = 0
+                    for n in self.neighbors((r,c)):
+                        r_n, c_n = n
+                        if self.tiles[r_n][c_n] == 0:
+                            num_water += 1
+                        elif self.tiles[r_n][c_n] == 1:
+                            num_grass += 1
+                    mountain_prob = num_grass / (num_grass + num_water)
+                    mountain_flag = random.choice([True] * int(mountain_prob * 100) + [False] * int((1 -mountain_prob) * 100))
+
+                    if mountain_flag:
+                        self.tiles[r][c] = 3
+
+    def neighbors(self, coord_pair):
+        r, c = coord_pair
+        neighbs = [(r + 1, c),
+                    (r - 1, c),
+                    (r, c - 1),
+                    (r, c + 1)]
+
+        for n_coord_pair in neighbs:
+            # Check bounds
+            r_n, c_n = n_coord_pair
+            if 0 <= r_n < self.size and 0 <= c_n < self.size:
+                yield n_coord_pair
+
 
 class Island:
+    # Play around with different models
     def __init__(self, r: int, c: int, grid: Grid):
-        # self.ocean_prob
         self.grid = grid
         self.core = r, c
 
@@ -52,55 +86,44 @@ class Island:
         self.generate()
 
     def generate(self) -> None:
-        # print(self.queue)
         self.grid.set(self.core[0], self.core[1], 1)
         
         # Enqueue valid neighbors and turn them into 1s
         while self.queue:
-            offset = 100
+            offset = 100 # REVIEW
             ocean_prob = 1/(1+(math.e ** (offset - len(self.tiles))))
-            # Review 100
-            # print(len(self.tiles))
-            # print(ocean_prob)
+
             target = self.queue.pop(0)
             self.process_neighbors(target, ocean_prob)
-            # print(self.queue)
-            # self.grid.print()
-            # time.sleep(0.01)
-            
 
-    def process_neighbors(self, coord_pair, ocean_prob):
+
+
+    def process_neighbors(self, coord_pair, ocean_prob) -> None:
         r, c = coord_pair
 
-        neighbors = [(r + 1, c),(r - 1, c),(r, c - 1),(r, c + 1)]
-        for i, n_coord_pair in enumerate(neighbors):
-            valid = True
-            # Check bounds
-            for coord in n_coord_pair:
-                if not (0 <= coord < self.grid.size):
-                    # Neighbor out of bounds
-                    valid = False
-                    break
-
-            if valid:
-                r_neighbor, c_neighbor = n_coord_pair 
-                # Water -> Sand
-                if self.grid.tiles[r_neighbor][c_neighbor] == 0:
-                    dist_from_core = math.sqrt((r-r_neighbor)**2 + (c-c_neighbor)**2)
-                    mod = 0.5 * (dist_from_core)
-                    shore_flag = random.choice([True] * int(mod * ocean_prob * 100) + [False] * int((1- mod * ocean_prob) * 100))
-                    if shore_flag:
-                        self.grid.set(r_neighbor, c_neighbor, 2)
-                    else:
-                        self.queue.append(n_coord_pair)
-                        self.tiles.append(n_coord_pair)
-                        self.grid.set(r_neighbor, c_neighbor, 1)
+        for n_coord_pair in self.grid.neighbors(coord_pair):
+            r_neighbor, c_neighbor = n_coord_pair 
+            # Water -> Sand
+            if self.grid.tiles[r_neighbor][c_neighbor] == 0:
+                dist_from_core = math.sqrt((r-r_neighbor)**2 + (c-c_neighbor)**2)
+                mod = 0.5 * (dist_from_core)
+                shore_flag = random.choice([True] * int(mod * ocean_prob * 100) + [False] * int((1- mod * ocean_prob) * 100))
+                if shore_flag:
+                    self.grid.set(r_neighbor, c_neighbor, 2)
+                else:
+                    self.queue.append(n_coord_pair)
+                    self.tiles.append(n_coord_pair)
+                    self.grid.set(r_neighbor, c_neighbor, 1)
+    
 
 
 if __name__ == "__main__":
-    g = Grid(50)    
-    g.add_island()
-    g.add_island()
+    g = Grid(400)    
+    i = 0
+    while i < 40:
+        g.generate_island()
+        i+=1
+    g.generate_mountains()
     g.print()
 
 
